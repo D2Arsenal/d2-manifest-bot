336149b5d415bbe5ccd0df574875d0a23761244f
Alexander Lichter <github@lichter.io>
2022-12-01 13:39:28 +0100

    feat: use to deploy netlify site

--- src/cron-check-for-manifest.ts
+++ src/cron-check-for-manifest.ts
@@ -1,73 +1,66 @@
 #!/usr/bin/env node
 
-import btoa from 'btoa';
-import fetch from 'cross-fetch';
-import { getDestinyManifest } from 'bungie-api-ts/destiny2';
-import { generateHttpClient } from '@d2api/manifest';
+import { getDestinyManifest, HttpClientConfig } from 'bungie-api-ts/destiny2';
 import latest from '../latest.json' assert { type: 'json' };
-import fse from 'fs-extra';
+import { writeFileSync } from 'fs-extra';
+import { $fetch } from 'ofetch';
 
-const { writeFileSync } = fse;
-const httpClient = generateHttpClient(fetch, process.env.API_KEY);
+
+const $http = async (config: HttpClientConfig) => $fetch(config.url, {
+  method: config.method,
+  params: config.params,
+  body: config.body,
+})
 
 const skipCheck = process.env.SKIP_CHECK === 'true' ? true : false;
 
 // do the thing
-(async () => {
-  const manifestMetadata = await getDestinyManifest(httpClient);
-
-  const current = manifestMetadata.Response.version;
-  const newREADME = `# d2-manifest-bot\ngithub action for checking for new d2 manifest\n\n# Current Manifest: ${current}`;
-
-  if (!skipCheck) {
-    console.log(`Latest:  ${latest}`);
-    console.log(`Current: ${current}`);
-    if (latest === current) {
-      // nothing changed. no updates needed.
-      return;
-    }
-    // if you are here, there's a new manifest
-    console.log('New manifest detected');
-
-    writeFileSync('latest.json', `${JSON.stringify(current, null, 2)}\n`, 'utf8');
-    writeFileSync('README.md', newREADME, 'utf8');
-  }
-
-  const buildMessage = `New manifest build - ${current}`;
-
-  // if (!/^[.\w-]+$/.test(versionNumber)) { I AM NOT REALLY SURE THIS NEEDS DOING. }
-
-  const buildOptions = {
-    url: 'https://api.github.com/repos/DestinyItemManager/d2-additional-info/dispatches',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: 'Basic ' + btoa(process.env.PAT || ''),
-      'User-Agent': 'd2-additional-info',
-    },
-    body: JSON.stringify({
-      event_type: 'new-manifest-detected',
-      client_payload: {
-        message: buildMessage,
-        branch: 'master',
-        config: {
-          env: {
-            MANIFEST_VERSION: current,
-          },
-        },
-      },
-    }),
-    json: true,
-    method: 'POST',
-  };
-  const githubFetch = await fetch(buildOptions.url, buildOptions);
-
-  if (!githubFetch.ok) {
-    console.log('Github returned an error');
-    console.log(githubFetch);
-    process.exit(1);
-  }
-})().catch((e) => {
+run().catch((e) => {
   console.log(e);
   process.exit(1);
 });
+
+async function run () {
+  const manifestMetadata = await getDestinyManifest($http);
+
+  const current = manifestMetadata.Response.version;
+
+  if (!skipCheck) {
+    writeNewManifestVersion(latest, current)
+  }
+
+  const { error } = await deployWebsite(current);
+
+  if (error) {
+    console.log('Github returned an error');
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+function writeNewManifestVersion (latest: string, current: string) {
+  const newREADME = `# d2-manifest-bot\ngithub action for checking for new d2 manifest\n\n# Current Manifest: ${current}`;
+  console.log(`Latest:  ${latest}`);
+  console.log(`Current: ${current}`);
+  const isNewManifest = latest !== current;
+  if (!isNewManifest) {
+    return;
+  }
+  console.log('New manifest detected');
+
+  writeFileSync('latest.json', `${JSON.stringify(current, null, 2)}\n`, 'utf8');
+  writeFileSync('README.md', newREADME, 'utf8');
+}
+
+function deployWebsite (current: string) {
+  const buildMessage = `New manifest build - ${current}`;
+
+  const buildOptions = {
+    url: process.env.NETLIFY_HOOK_URL!,
+    query: {
+      trigger_title: buildMessage,
+    },
+    method: 'POST',
+  };
+  return $fetch(buildOptions.url, buildOptions);
+}
